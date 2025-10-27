4d7c84f5907a5cd3c4decb4e6e628da752862ee7
John Jenrick Renes <jenrickrenes1@gmail.com>
2025-10-27 23:47:15 +0800

    Multiply function

--- calculator2.0.js
+++ calculator2.0.js
@@ -6,8 +6,13 @@ function subtract(a, b) {
     return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 let a = 7;
 let b = 3;
 
 console.log("Add : " + add(a, b));
 console.log("Subtract : ", subtract(a, b));
+console.log("Multiply: " + multiply(x, y));
