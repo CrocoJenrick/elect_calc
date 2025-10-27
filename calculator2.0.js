d9179c6e662b76eb10bc77a054ac765b45f4466f
John Jenrick Renes <jenrickrenes1@gmail.com>
2025-10-27 23:50:02 +0800

    Divide function

--- calculator2.0.js
+++ calculator2.0.js
@@ -10,9 +10,14 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    return a / b;
+}
+
 let a = 7;
 let b = 3;
 
 console.log("Add : " + add(a, b));
 console.log("Subtract : ", subtract(a, b));
 console.log("Multiply: " + multiply(x, y));
+console.log("Divide: " + divide(x, y));
