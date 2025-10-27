cdea4302222e17a5aa614e5fd776d53388614e03
John Jenrick Renes <jenrickrenes1@gmail.com>
2025-10-27 23:45:04 +0800

    Subtraction function

--- calculator2.0.js
+++ calculator2.0.js
@@ -2,7 +2,12 @@ function add(a, b) {
   return a + b;
 }
 
+function subtract(a, b) {
+    return a - b;
+}
+
 let a = 7;
 let b = 3;
 
 console.log("Add : " + add(a, b));
+console.log("Subtract : ", subtract(a, b));
